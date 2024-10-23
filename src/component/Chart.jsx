5f9df37980d9a922e629ada7726b263718a34e05
Sina <sinasadeghi070@gmail.com>
2024-10-23 10:51:10 +0330

    tow in one bar

--- src/component/Chart.jsx
+++ src/component/Chart.jsx
@@ -1,4 +1,4 @@
-import {Bar, CartesianGrid, ComposedChart, Legend, Line, Tooltip, XAxis, YAxis} from "recharts";
+import {Bar, CartesianGrid, Cell, ComposedChart, Legend, Line, Tooltip, XAxis, YAxis} from "recharts";
 
 export default function Chart({dataChart, setDataChart}) {
     console.log(dataChart)
@@ -13,7 +13,24 @@ export default function Chart({dataChart, setDataChart}) {
                 <Tooltip />
                 <Legend />
                 <CartesianGrid stroke="#f5f5f5" />
-                <Bar yAxisId="left" name={'Buy Amount (USD)'} dataKey="Buy Amount (USD)" barSize={30} fill="#8884d8" />
+
+                <Bar yAxisId="left" name={'Buy Amount (USD)'} dataKey="Buy Amount (USD)" stackId={'stackedBars'} barSize={30} fill="#8884d8" >
+                    {dataChart?.HotTokenHolders.map((entry, index) => (
+                        <Cell
+                            key={`cell-${index}`}
+                            fill={entry['Buy Amount (USD)'] > entry['Sell Amount (USD)'] ? '#bb2222' : '#00f851'}
+                        />
+                    ))}
+                </Bar>
+                <Bar yAxisId="left" name={'Sell Amount (USD)'} dataKey="Sell Amount (USD)" stackId={'stackedBars'} barSize={30} fill="#82ca9d">
+                    {dataChart?.HotTokenHolders.map((entry, index) => (
+                        <Cell
+                            key={`cell-${index}`}
+                            fill={entry['Buy Amount (USD)'] > entry['Sell Amount (USD)'] ? '#dd3333' : '#00c851'}
+                        />
+                    ))}
+                </Bar>
+
                 <Line yAxisId="right" name={'Total buy sell times'} type="monotone" dataKey={(d) => d?.['Num of Buy Times'] + d?.['Num of Sell Times']} stroke="#ff7300" />
             </ComposedChart >
         </div>
